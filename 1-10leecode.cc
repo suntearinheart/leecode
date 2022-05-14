fe96e5213c7250baa77beb1daee65ca8e9e7e249
Ares Protocol Challenger <86470140+Aresprotocol@users.noreply.github.com>
2022-05-14 10:14:30 +0800

    add the gitigonre

--- 1-10leecode.cc
+++ 1-10leecode.cc
@@ -3,7 +3,7 @@
 #include <map>
 using std::map;
 using std::vector;
-
+// big(N)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -11,12 +11,13 @@ public:
         map<int, int> map_tt; // value and index
         int index = 0;
         while (index < nums.size()) {
-            
+            // find the pair
             if (map_tt.find(target - nums[index]) != map_tt.end()) {               
                 pairs.push_back(map_tt.find(target - nums[index])->second);
                 pairs.push_back(index);
                 return pairs;
             } else {
+                // if failed, add to map
                 map_tt.insert(std::pair<int,int>(nums[index],index));
             }
             index++;
@@ -24,6 +25,20 @@ public:
         return pairs;
     }
 };
-
-
-
+// big(N*N)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> pairs;
+        for(int index = 0; index < nums.size(); index++) {
+            for(int index1 = index+1; index1 < nums.size(); index1++) {
+                if ((nums[index1] + nums[index]) == target) {
+                    pairs.push_back(index);
+                    pairs.push_back(index1);
+                    return pairs;
+                }
+            }
+        }
+     return pairs;       
+    }
+};
